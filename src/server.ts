454033372234a0976185dccdb71d84cffde8c994
Mike Bauer <mikeb@parsed.io>
2021-02-06 14:53:44 -0500

    add route to upload, filter, and delete image

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {
+  filterImageFromURL,
+  deleteLocalFiles,
+  validURL,
+} from './util/util';
 
 (async () => {
 
@@ -12,31 +16,33 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
+  // route to get image from public url, filter it, save to disk, then erase from disk.
+  // get /filteredimage?image_url={{URL}}
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    const image_url  = req.query.image_url as string;
+
+    if (!image_url) {
+      return res.status(400).send('request must have contain a query string ?image_url={{URL}}')
+    }
+    const isValidUrl: boolean = validURL(image_url)
+    if(!isValidUrl) {
+      return res.status(400).send('Invalid Url')
+    }
+    try {
+      const filteredPath = await filterImageFromURL(image_url)
+      deleteLocalFiles([filteredPath])
+      res.status(200).send(filteredPath)
+    } catch (error) {
+      res.status(500).send({error, message: 'There was a problem filtering or saving your file. Make sure the image url provided is valid.'})
+    }
+  })
 
   // Start the Server
   app.listen( port, () => {
