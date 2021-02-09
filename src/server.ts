2ffa729412125e290d3595ec4eae776da49b4d92
Mike Bauer <mikeb@parsed.io>
2021-02-08 19:27:35 -0500

    comments and minor housekeeping

--- src/server.ts
+++ src/server.ts
@@ -44,7 +44,7 @@ import * as aws from './aws'
 
   //route to optionally filter image and then upload to S3 bucket
   //optional body param {filter: filter_type}
-  //  fiter_type - 'greyscale' | 'sepia'  no filter if not added
+  //  filter_type - 'greyscale' | 'sepia'  no filter if not added
   app.post("/filteredimage/upload", async (req: Request, res: Response) => {
     const image_url  = req.query.image_url as string;
     const { filter } = req.body
@@ -72,9 +72,8 @@ import * as aws from './aws'
     }
   })
 
-  // route to get image from public url, filter it, save to disk, then erase from disk.
-  // get /filteredimage?image_url={{URL}}
-  //route to get a specific image from s3 bucket
+  // route to get and return preSigned Get URl from S3.
+  //returns the presignedURL which can be used to download image from S3
   app.get( "/filteredimage/:file_name", async (req: Request, res: Response) => {
     const { file_name } = req.params;
     try {
